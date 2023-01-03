fb56f6e536c22ec98b0f5c5d2af37c970953388b
DhGilGar <110663176+DhGilGar@users.noreply.github.com>
2023-01-03 10:42:04 +0100

    Add files via upload

--- JS/functions.js
+++ JS/functions.js
@@ -1,21 +1,8 @@
 $(document).ready(function(){
-    $(".dropdown").hover(            
+    $(".dropdown").click(            
         function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideToggle("400");
             $(this).toggleClass('open');        
-        },
-        function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-            $(this).toggleClass('open');       
         }
     );
 });
-    //RESALTA LOS PUNTOS
-    let elementSel = document.getElementsByClassName("listslider")[0].getElementsByTagName("a");
-    elementSel[curElement].classList.remove("item-select-slid");
-    elementSel[nextElement].classList.add("item-select-slid");
-    elements[curElement].style.opacity=0;
-    elements[curElement].style.zIndex =0;
-    elements[nextElement].style.opacity=1;
-    elements[nextElement].style.zIndex =1;
-}
